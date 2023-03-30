89c48e695f4c77d0810dbd498b90eb339c0fd605
VictorIzek <victor_almanzap@outlook.com>
2023-03-30 09:01:16 -0600

    asdad

--- contracts/auth.ts
+++ contracts/auth.ts
@@ -5,7 +5,7 @@
  * file.
  */
 
-import UserUser from 'App/Models/UserUser'
+import User from 'App/Models/user/User'
 
 declare module '@ioc:Adonis/Addons/Auth' {
   /*
@@ -34,8 +34,8 @@ declare module '@ioc:Adonis/Addons/Auth' {
     |
     */
     user: {
-      implementation: LucidProviderContract<typeof UserUser>
-      config: LucidProviderConfig<typeof UserUser>
+      implementation: LucidProviderContract<typeof User>
+      config: LucidProviderConfig<typeof User>
     }
   }
 
